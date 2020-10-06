513e59d25ea2b367d5bf6c92d43f27e94e0a2435
Aditya Verma <aaadarths2357@gmail.com>
2020-10-06 19:22:19 +0530

    Refactor: find cheapest hotel based on weekend and weekday for a given date range

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelOperation.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelOperation.java
@@ -1,9 +1,10 @@
 package com.bridgelabz.hotelreservationsystem;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
@@ -40,28 +41,46 @@ public class HotelOperation {
 		}
 	}
 
-//	public void findCheapestHotel() throws DateTimeParseException {
-//
-//		System.out.println("Enter start date and end date int the format (yyyy-MM-dd),(yyyy-MM-dd)");
-//		String line=SC.next();
-//		String[] input=line.split(",");
-//		
-//		LocalDate startDate = LocalDate.parse(input[0]);
-//		LocalDate endDate = LocalDate.parse(input[1]);
-//		
-//		int dateDifference=(int)ChronoUnit.DAYS.between(startDate, endDate);
-//		int cheapestRate=999999999;
-//		String cheapestHotel="";
-//		
-//		for(Hotel hotel : hotelList) {
-//			int rateForHotel=dateDifference*hotel.getWeekdayRateForRegularCustomer();
-//			if(rateForHotel<cheapestRate) {
-//				cheapestRate=rateForHotel;
-//				cheapestHotel=hotel.getHotelName();
-//			}
-//		}
-//		if(cheapestRate!=999999999)
-//			System.out.println("Cheapest Hotel : \n"+cheapestHotel+", Total Rates: "+cheapestRate);
-//	}
+	public void findCheapestHotel() throws DateTimeParseException {
+
+		System.out.println("Enter start date and end date int the format (yyyy-MM-dd),(yyyy-MM-dd)");
+		String line = SC.next();
+		String[] input = line.split(",");
+
+		LocalDate startDate = LocalDate.parse(input[0]);
+		LocalDate endDate = LocalDate.parse(input[1]);
+		endDate = endDate.plusDays(1);
+
+		int totalDateDifference = (int) ChronoUnit.DAYS.between(startDate, endDate);
+		System.out.println("totalDateDifference  " + totalDateDifference);
+		int cheapestRate = 999999999;
+		String cheapestHotel = "";
+		int noOfWeekdends = 0;
+		int noOfWeekdays = 0;
+
+		while (startDate.compareTo(endDate) != 0) {
+			switch (DayOfWeek.of(startDate.get(ChronoField.DAY_OF_WEEK))) {
+			case SATURDAY:
+				++noOfWeekdends;
+				break;
+			case SUNDAY:
+				++noOfWeekdends;
+				break;
+			}
+			startDate = startDate.plusDays(1);
+		}
+
+		noOfWeekdays = totalDateDifference - noOfWeekdends;
+		for (Hotel hotel : hotelList) {
+			int rateForHotel = (noOfWeekdays * hotel.getWeekdayRateForRegularCustomer())
+					+ (noOfWeekdends * hotel.getWeekendRateForRegularCustomer());
+			if (rateForHotel < cheapestRate) {
+				cheapestRate = rateForHotel;
+				cheapestHotel = hotel.getHotelName();
+			}
+		}
+		if (cheapestRate != 999999999)
+			System.out.println("Cheapest Hotel : \n" + cheapestHotel + ", Total Rates: " + cheapestRate);
+	}
 
 }
