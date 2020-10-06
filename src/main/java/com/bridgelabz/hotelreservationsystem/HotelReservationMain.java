9dcce253f5dd29fe4d517383f5b57289073bb9e4
Aditya Verma <aaadarths2357@gmail.com>
2020-10-06 16:05:31 +0530

    Added: first welcome to hotel system program to test program configuration

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class HotelReservationMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservatin System");
+	}
+
+}
